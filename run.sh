63dacd5f966f08eb104b627ca88e33aeea82f6d5
Cloud User <centos@ip-172-20-1-41.ec2.internal>
2020-12-13 10:28:26 +0000

    Simple-Web APP Initial commits

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+read -p "Enter Application Name:" appName
+
+echo "********************************************************"
+echo "  Launching Helm Chart "
+echo "********************************************************"
+
+helm install ${appName} $PWD/hello-world/
+
+
+echo "*********************************************************"
+echo "  Please be patient while the chart is being deployed"
+echo "*********************************************************"
+
+sleep 30
+
+echo "**********************************************************"
+echo "  ${appName}  Launched "
+echo "**********************************************************"
+
+
+echo "**********************************************************"
+echo "  Use Following URL To Launch The App On Browser "
+echo "**********************************************************"
+
+minikube service ${appName}-wordpress
+
+
+
